be36f12945f964820106d790ee102a40691ea094
OlanrewajuEniola <olaeniola16@gmail.com>
2021-04-26 10:02:05 +0100

    'AND', 'OR' & 'NOT' Operators.

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-// Tenary Operators
+// Logical Operators
 
-//If a customer has more than 100 points
-//he is a 'gold' customer, otherwise
-//he is a 'silver' customer.
+//Logical AND (&&). It returns TRUE
+//if both operands are TRUE.
 
-let points = 90;
+console.log(true && true);
 
-let type = points > 100 ? 'Gold' : 'Silver';
+console.log(false && true);
 
-console.log(type);
+// Real life example:
+//let highIncome = true;
+//let goodCreditScore = true;
+//let eligibleForLoan = highIncome && goodCreditScore;
+
+//console.log(eligibleForLoan);
+
+
+//Logical OR (!!). It returns TRUE if one of 
+//the operands is TRUE.
+
+let highIncome = false;
+let goodCreditScore = false;
+let eligibleForLoan = highIncome || goodCreditScore;
+console.log('Eligible', eligibleForLoan);
+
+let applicationRefused = !eligibleForLoan;
+console.log('Application Refused', applicationRefused);
+
 
 
 
