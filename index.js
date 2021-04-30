ba2dbcbf0e40f1cda99fd9d8b979bbdaee39d744
OlanrewajuEniola <olaeniola16@gmail.com>
2021-04-30 12:15:25 +0100

    LogicalColorWithNonBoolean.

--- index.js
+++ index.js
@@ -1,38 +1,8 @@
-// Logical Operators
+let userColor = 'Red';
+let defaultColor = 'Blue';
+let currentColor = userColor || defaultColor;
 
-//Logical AND (&&). It returns TRUE
-//if both operands are TRUE.
-
-console.log(true && true);
-
-console.log(false && true);
-
-// Real life example:
-//let highIncome = true;
-//let goodCreditScore = true;
-//let eligibleForLoan = highIncome && goodCreditScore;
-
-//console.log(eligibleForLoan);
-
-
-//Logical OR (!!). It returns TRUE if one of 
-//the operands is TRUE.
-
-let highIncome = false;
-let goodCreditScore = false;
-let eligibleForLoan = highIncome || goodCreditScore;
-console.log('Eligible', eligibleForLoan);
-
-let applicationRefused = !eligibleForLoan;
-console.log('Application Refused', applicationRefused);
-
-
-
-
-
-
-
-
+console.log(currentColor);
 
 
 
